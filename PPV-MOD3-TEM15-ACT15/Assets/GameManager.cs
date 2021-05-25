8231abfcbf49c65902c778b96f724491835b4058
Adrián Rangel Suárez <arangelsuarez@hotmail.com>
2021-05-25 16:54:03 -0500

    added lfs support

---
+++ PPV-MOD3-TEM15-ACT15/Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject house1, house2, house3;
+
+    public void Button1()
+    {
+        house1.SetActive(!house1.activeSelf);
+    }
+
+    public void Button2()
+    {
+        house2.SetActive(!house2.activeSelf);
+    }
+
+    public void Button3()
+    {
+        house3.SetActive(!house3.activeSelf);
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Vuforia Test Project
+
+This repository includes:
+* Source project
+* APK
+* Image target for download and / or printing
+
+_by ARS aka. The Developer_
